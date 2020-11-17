71ff6f539364e04a480eb6e06a45f78abb22b457
rryb77 <rryb77@gmail.com>
2020-11-17 11:49:35 -0600

    added an array to store fish as objects

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,26 @@
+// Adding to the fish collection via JS instead of hardcoded HTML
+
+// A fish array storing the collection as objects 
+const fishCollection = [
+    {
+        name: "Flounder",
+        species: "Flounder",
+        length: "6 inches",
+        location: "Disney World",
+        food: "Guppy Food"        
+    }
+    {
+        name: "Blinky",
+        species: "Uknown",
+        length: "3 feet",
+        location: "Nuclear Pond",
+        food: "Souls"        
+    }
+    {
+        name: "Rainbow Fish",
+        species: "Fictional",
+        length: "2 1/2 Feet",
+        location: "Library",
+        food: "Colors"        
+    }
+]
