257754e30dbd89c2046f2626975472a7152f2679
rryb77 <rryb77@gmail.com>
2020-11-18 10:43:59 -0600

    Automated the tank tips via javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 import { FishList } from './fish/FishList.js'
-
+import { TipList } from './tips/TipList.js'
 //call FishList to get everything started once index.html is loaded
 FishList()
+TipList()
